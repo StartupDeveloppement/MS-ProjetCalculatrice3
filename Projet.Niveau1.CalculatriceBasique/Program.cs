08b503537c0a5ff83358e74e022383cf9fd79729
Sarah Gumaa <sarahgumaa55@gmail.com>
2015-10-30 20:11:56 -0500

    Premier commit

--- Projet.Niveau1.CalculatriceBasique/Program.cs
+++ Projet.Niveau1.CalculatriceBasique/Program.cs
@@ -5,30 +5,37 @@ namespace Projet.Niveau1.CalculatriceBasique
     internal class Program
     {
         private static void Main(string[] args)
-        {
-            //Essaie
-            //Console.ForegroundColor = ConsoleColor.Green;
-            //Affichage.AfficherSansRetourLigne("\nLe tarif essence donne : ");
-           
-            Affichage.Afficher("Bonjour !");
+        { 
+            CentrerLeTexte("Bonjour!");
             TypeOperation();
         }
-
+        private static void CentrerLeTexte(string texte)
+        {
+            int nbEspaces = (Console.WindowWidth - texte.Length) / 2;
+            Console.SetCursorPosition(nbEspaces, Console.CursorTop);
+            Affichage.Afficher(texte);
+        }
         private static void TypeOperation()
         {
-            Console.ForegroundColor = ConsoleColor.Pink;
+            Console.ForegroundColor = ConsoleColor.White;
             Affichage.Afficher("Quelle type d'opération souhaitez vous faire :");
             Console.ForegroundColor = ConsoleColor.Blue;
             Affichage.Afficher("1.Addition");
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Affichage.Afficher("2.Soustraction");
-			Affichage.Afficher("3.Division");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Affichage.Afficher("3.Division");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Affichage.Afficher("4.Multiplication");
 
+
+            Console.ForegroundColor = ConsoleColor.White;
             int saisieClavier = Convert.ToInt16(Console.ReadLine());
+           
 
             if (saisieClavier == 1)
             {
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Affichage.AfficherSansRetourLigne("Premier nombre :");
                 double premierChiffre = Convert.ToDouble(Console.ReadLine());
                 Affichage.AfficherSansRetourLigne("Second nombre :");
@@ -37,52 +44,55 @@ namespace Projet.Niveau1.CalculatriceBasique
                 var calcul = new Calcul(premierChiffre, secondChiffre);
                 var resultat = calcul.addition();
 
-
+                Console.ForegroundColor = ConsoleColor.Green;
                 Affichage.Afficher("Le résultat est : " + resultat);
                 TypeOperation();
             }
 			
 			else if (saisieClavier==2)
 			{
-					Affichage.AfficherSansRetourLigne("Premier nombre :");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Affichage.AfficherSansRetourLigne("Premier nombre :");
 				    double premierChiffre = Convert.ToDouble(Console.ReadLine());
 					Affichage.AfficherSansRetourLigne("Second nombre :");
 					double secondChiffre = Convert.ToDouble(Console.ReadLine());
 
 					var calcul = new Calcul(premierChiffre, secondChiffre);
 					var resultat = calcul.soustraction();
-
-					Affichage.Afficher("Le résultat est : " + resultat);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Affichage.Afficher("Le résultat est : " + resultat);
 					TypeOperation();
 			
 			}
 			
 			else if (saisieClavier==3)
 			{
-					Affichage.AfficherSansRetourLigne("Premier nombre :");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Affichage.AfficherSansRetourLigne("Premier nombre :");
 					double premierChiffre = Convert.ToDouble(Console.ReadLine());
 					Affichage.AfficherSansRetourLigne("Second nombre :");
 					double secondChiffre = Convert.ToDouble(Console.ReadLine());
 
 					var calcul = new Calcul(premierChiffre, secondChiffre);
 					var resultat = calcul.division();
-
-					Affichage.Afficher("Le résultat est : " + resultat);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Affichage.Afficher("Le résultat est : " + resultat);
 					TypeOperation();
 			
 			}
 			
 			else if (saisieClavier==4)
 			{
-					Affichage.AfficherSansRetourLigne("Premier nombre :");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Affichage.AfficherSansRetourLigne("Premier nombre :");
 					double premierChiffre = Convert.ToDouble(Console.ReadLine());
 					Affichage.AfficherSansRetourLigne("Second nombre :");
 					double secondChiffre = Convert.ToDouble(Console.ReadLine());
 
 					var calcul = new Calcul(premierChiffre, secondChiffre);
 					var resultat = calcul.Multiplication();
-
-					Affichage.Afficher("Le résultat est : " + resultat);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Affichage.Afficher("Le résultat est : " + resultat);
 					TypeOperation();
 			
 			}
@@ -90,6 +100,7 @@ namespace Projet.Niveau1.CalculatriceBasique
 			
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Affichage.Afficher("Mauvaise saisie");
                 TypeOperation();
             }

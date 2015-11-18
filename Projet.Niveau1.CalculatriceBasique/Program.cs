572c153a1b7e1a5733c5a4124c258a59fd880208
Sarah Gumaa <sarahgumaa55@gmail.com>
2015-11-18 17:08:28 -0600

    Couleur, ajout d' un nouveau operateur, gestion des exception

--- Projet.Niveau1.CalculatriceBasique/Program.cs
+++ Projet.Niveau1.CalculatriceBasique/Program.cs
@@ -4,106 +4,102 @@ namespace Projet.Niveau1.CalculatriceBasique
 {
     internal class Program
     {
+        private static object secondChiffre;
+
         private static void Main(string[] args)
-        { 
-            CentrerLeTexte("Bonjour!");
+        {
+            Affichage.Afficher("Bonjour !");
             TypeOperation();
         }
-        private static void CentrerLeTexte(string texte)
-        {
-            int nbEspaces = (Console.WindowWidth - texte.Length) / 2;
-            Console.SetCursorPosition(nbEspaces, Console.CursorTop);
-            Affichage.Afficher(texte);
-        }
+
+      
+
         private static void TypeOperation()
         {
-            Console.ForegroundColor = ConsoleColor.White;
             Affichage.Afficher("Quelle type d'opération souhaitez vous faire :");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Affichage.Afficher("1.Addition");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Affichage.Afficher("2.Soustraction");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Affichage.Afficher("3.Division");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Affichage.Afficher("4.Multiplication");
-
+         
+            Affichage.AffichageCouleur("1.Addition", ConsoleColor.Yellow);
+            Affichage.AffichageCouleur("2.Soustraction", ConsoleColor.Blue);
+			Affichage.AffichageCouleur("3.Division", ConsoleColor.Green);
+            Affichage.AffichageCouleur("4.Multiplication", ConsoleColor.Red);
 
             Console.ForegroundColor = ConsoleColor.White;
-            int saisieClavier = Convert.ToInt16(Console.ReadLine());
-           
-
-            if (saisieClavier == 1)
+            double premierChiffre = 0.0;
+            double secondChiffre = 0.0;
+            Calcul calcul = new Calcul(premierChiffre, secondChiffre);
+            double resultat;
+            //try parse
+            string saisieClavier = Console.ReadLine();
+            int result;
+            bool resultatParse = int.TryParse(saisieClavier, out result);
+            if (!resultatParse)
             {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Affichage.AfficherSansRetourLigne("Premier nombre :");
-                double premierChiffre = Convert.ToDouble(Console.ReadLine());
-                Affichage.AfficherSansRetourLigne("Second nombre :");
-                double secondChiffre = Convert.ToDouble(Console.ReadLine());
-
-                var calcul = new Calcul(premierChiffre, secondChiffre);
-                var resultat = calcul.addition();
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Affichage.Afficher("Le résultat est : " + resultat);
+                Affichage.Afficher("la saisie n'est pas numerique, recommencez");
                 TypeOperation();
             }
-			
-			else if (saisieClavier==2)
-			{
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Affichage.AfficherSansRetourLigne("Premier nombre :");
-				    double premierChiffre = Convert.ToDouble(Console.ReadLine());
-					Affichage.AfficherSansRetourLigne("Second nombre :");
-					double secondChiffre = Convert.ToDouble(Console.ReadLine());
-
-					var calcul = new Calcul(premierChiffre, secondChiffre);
-					var resultat = calcul.soustraction();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Affichage.Afficher("Le résultat est : " + resultat);
-					TypeOperation();
-			
-			}
-			
-			else if (saisieClavier==3)
-			{
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Affichage.AfficherSansRetourLigne("Premier nombre :");
-					double premierChiffre = Convert.ToDouble(Console.ReadLine());
-					Affichage.AfficherSansRetourLigne("Second nombre :");
-					double secondChiffre = Convert.ToDouble(Console.ReadLine());
-
-					var calcul = new Calcul(premierChiffre, secondChiffre);
-					var resultat = calcul.division();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Affichage.Afficher("Le résultat est : " + resultat);
-					TypeOperation();
-			
-			}
-			
-			else if (saisieClavier==4)
-			{
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Affichage.AfficherSansRetourLigne("Premier nombre :");
-					double premierChiffre = Convert.ToDouble(Console.ReadLine());
-					Affichage.AfficherSansRetourLigne("Second nombre :");
-					double secondChiffre = Convert.ToDouble(Console.ReadLine());
-
-					var calcul = new Calcul(premierChiffre, secondChiffre);
-					var resultat = calcul.Multiplication();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Affichage.Afficher("Le résultat est : " + resultat);
-					TypeOperation();
-			
-			}
-			
-			
             else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Affichage.Afficher("Mauvaise saisie");
-                TypeOperation();
+            { 
+                switch (result)
+                {
+                case 1:
+                    Affichage.AfficherSansRetourLigne("Premier nombre :");
+                    premierChiffre = Convert.ToDouble(Console.ReadLine());
+                    Affichage.AfficherSansRetourLigne("Second nombre :");
+                    secondChiffre = Convert.ToDouble(Console.ReadLine());
+
+                    calcul = new Calcul(premierChiffre, secondChiffre);
+                    resultat = calcul.addition();
+                    Affichage.Afficher("Le résultat est : " + resultat);
+                    TypeOperation();
+                    break;
+
+                case 2:
+                    Affichage.AfficherSansRetourLigne("Premier nombre :");
+                    premierChiffre = Convert.ToDouble(Console.ReadLine());
+                    Affichage.AfficherSansRetourLigne("Second nombre :");
+                    secondChiffre = Convert.ToDouble(Console.ReadLine());
+                    calcul = new Calcul(premierChiffre, secondChiffre);
+                    resultat = calcul.soustraction();
+                    Affichage.Afficher("Le résultat est : " + resultat);
+                    TypeOperation();
+                    break;
+                case 3:
+                    Affichage.AfficherSansRetourLigne("Premier nombre :");
+                    premierChiffre = Convert.ToDouble(Console.ReadLine());
+                    Affichage.AfficherSansRetourLigne("Second nombre :");
+                    secondChiffre = Convert.ToDouble(Console.ReadLine());
+
+                    calcul = new Calcul(premierChiffre, secondChiffre);
+                    resultat = calcul.division();
+
+                    Affichage.Afficher("Le résultat est : " + resultat);
+                    TypeOperation();
+                    break;
+                case 4:
+                    Affichage.AfficherSansRetourLigne("Premier nombre :");
+                    premierChiffre = Convert.ToDouble(Console.ReadLine());
+                    Affichage.AfficherSansRetourLigne("Second nombre :");
+                    secondChiffre = Convert.ToDouble(Console.ReadLine());
+
+                    calcul = new Calcul(premierChiffre, secondChiffre);
+                    resultat = calcul.Multiplication();
+
+                    Affichage.Afficher("Le résultat est : " + resultat);
+                    TypeOperation();
+                    break;
+
+                    default:
+                    Affichage.Afficher("Mauvaise saisie");
+                    TypeOperation();
+                    break;
+                }
             }
-        }
+			
+	
+			
+			
+			
+            
+       } 
     }
 }
